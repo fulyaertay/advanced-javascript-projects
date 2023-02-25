9a0d29c914bde9ef60fddcb7d37f9ffca19b1129
Fulya <fulya.ertay@gmail.com>
2023-02-25 12:17:04 +0300

    first init

---
+++ 1-role-playing-game/index.js
@@ -0,0 +1,7 @@
+/*
+Challenge: 
+1. Take the hard-coded HTML for the Wizard card, bring it 
+   into index.js and then inject it back into its div with 
+   JavaScript.
+2. Do the same for Orc card. 
+*/
