bb405045b6305292c13841941386c1dc0ebf0cc6
Fulya <fulya.ertay@gmail.com>
2023-02-25 19:47:34 +0300

    added getCharacterHtml() function

--- 1-role-playing-game/index.js
+++ 1-role-playing-game/index.js
@@ -1,13 +1,7 @@
 function getDiceRollArray(diceCount) {
-   return new Array(diceCount).fill(0).map(function () {
-       return Math.floor(Math.random() * 6) + 1
-   });
-}
-
-function getDiceHtml(diceCount) {
-   return getDiceRollArray(diceCount).map(function (num) {
-       return `<div class="dice">${num}</div>`
-   }).join('')
+   return new Array(diceCount).fill(0).map(function(){
+   return Math.floor(Math.random() * 6) + 1
+   });   
 }
 
 const hero = {
@@ -26,45 +20,38 @@ const monster = {
    diceCount: 1
 }
 
-/*
-Challenge
-1. Create a method called getCharacterHtml that performs the 
-  same tasks as our current renderCharacter function.
-2. Create two new instances of Character. One for a hero, 
-  called "wizard", and one for a monster, called "orc". 
-  Render both of them on the page.
-3. Delete both the old renderCharacter function and the two 
-  lines of code at the bottom of the page which invoke that 
-  function.
-*/
-
 function Character(data) {
-   this.elementId = data.elementId;
-   this.name = data.name;
-   this.avatar = data.avatar;
-   this.health = data.health;
-   this.diceCount = data.diceCount;
-
-   this.getCharacterHtml = function () {
-       const { elementId, name, avatar, health, diceCount } = this;
-       const diceHtml = getDiceHtml(diceCount)
-
-       document.getElementById(elementId).innerHTML =
-           `<div class="character-card">
-           <h4 class="name"> ${name} </h4>
-           <img class="avatar" src="${avatar}" />
-           <div class="health">health: <b> ${health} </b></div>
-           <div class="dice-container">    
-               ${diceHtml}
-           </div>
-       </div>`;
-
+   Object.assign(this, data)
+   
+    this.getDiceHtml = function(diceCount) {
+       return getDiceRollArray(diceCount).map(function(num){ 
+           return  `<div class="dice">${num}</div>`
+       }).join('')
    }
 
+   this.getCharacterHtml = function () {
+       const { elementId, name, avatar, health, diceCount } = this;      
+       let diceHtml = this.getDiceHtml(diceCount);
+       
+          return `
+           <div class="character-card">
+               <h4 class="name"> ${name} </h4>
+               <img class="avatar" src="${avatar}" />
+               <div class="health">health: <b> ${health} </b></div>
+               <div class="dice-container">
+                   ${diceHtml}
+               </div>
+           </div>`;
+   }  
 }
 
+//CHALLENGE
+// - Make the wizard and orc render
+// - check hint.md if you need a hand 
+
 const wizard = new Character(hero)
-const orc = new Character(monster)
 wizard.getCharacterHtml()
+
+const orc = new Character(monster)
 orc.getCharacterHtml()
 
