6a96f56a172b4b4aaad61ace9c227ed5556b29c6
Fulya <fulya.ertay@gmail.com>
2023-02-25 22:01:56 +0300

    updated Character constructor

--- 1-role-playing-game/index.js
+++ 1-role-playing-game/index.js
@@ -1,57 +1,11 @@
-function getDiceRollArray(diceCount) {
-   return new Array(diceCount).fill(0).map(function(){
-   return Math.floor(Math.random() * 6) + 1
-   });   
+import characterData from './data.js'
+import Character from './Character.js'
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
 }
 
-const hero = {
-   elementId: "hero",
-   name: "Wizard",
-   avatar: "images/wizard.png",
-   health: 60,
-   diceCount: 3
-}
-
-const monster = {
-   elementId: "monster",
-   name: "Orc",
-   avatar: "images/orc.png",
-   health: 10,
-   diceCount: 1
-}
-
-function Character(data) {
-   Object.assign(this, data)
-   
-    this.getDiceHtml = function(diceCount) {
-       return getDiceRollArray(diceCount).map(function(num){ 
-           return  `<div class="dice">${num}</div>`
-       }).join('')
-   }
-
-   this.getCharacterHtml = function () {
-       const { elementId, name, avatar, health, diceCount } = this;      
-       let diceHtml = this.getDiceHtml(diceCount);
-       
-          return `
-           <div class="character-card">
-               <h4 class="name"> ${name} </h4>
-               <img class="avatar" src="${avatar}" />
-               <div class="health">health: <b> ${health} </b></div>
-               <div class="dice-container">
-                   ${diceHtml}
-               </div>
-           </div>`;
-   }  
-}
-
-//CHALLENGE
-// - Make the wizard and orc render
-// - check hint.md if you need a hand 
-
-const wizard = new Character(hero)
-wizard.getCharacterHtml()
-
-const orc = new Character(monster)
-orc.getCharacterHtml()
-
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
+render()
