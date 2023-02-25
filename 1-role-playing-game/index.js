b5ddde5763c135c258e9dfea7399aa5b4c5526f2
Fulya <fulya.ertay@gmail.com>
2023-02-25 16:09:14 +0300

    added getDiceRollArray() function

--- 1-role-playing-game/index.js
+++ 1-role-playing-game/index.js
@@ -1,25 +1,45 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+function getDiceRollArray(diceCount) {
+   return new Array(diceCount).fill(0).map(function(){
+       return Math.floor(Math.random() * 6) + 1
+   })    
+}
 
-document.getElementById('hero').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Wizard </h4>
-        <img class="avatar" src="images/wizard.png"/>
-        <p class="health">health: <b> 60 </b></p>
-        <div class="dice-container"><div class="dice"> 6 </div></div>
-    </div>   
-`
+function getDiceHtml(diceCount) {
+   return getDiceRollArray(diceCount).map(function(num){ 
+       return  `<div class="dice">${num}</div>`
+   }).join('')
+}
 
-document.getElementById('monster').innerHTML = `
-    <div class="character-card">
-        <h4 class="name"> Orc </h4>
-        <img class="avatar" src="images/orc.png"/>
-        <p class="health">health: <b> 10 </b></p>
-        <div class="dice-container"><div class="dice"> 4 </div></div>
-    </div>
-`
+const hero = {
+   elementId: "hero",
+   name: "Wizard",
+   avatar: "images/wizard.png",
+   health: 60,
+   diceCount: 3
+}
+
+const monster = {
+   elementId: "monster",
+   name: "Orc",
+   avatar: "images/orc.png",
+   health: 10,
+   diceCount: 1
+}
+
+function renderCharacter(data) {
+   const { elementId, name, avatar, health, diceCount } = data;
+   const diceHtml = getDiceHtml(diceCount)
+
+   document.getElementById(elementId).innerHTML =
+       `<div class="character-card">
+           <h4 class="name"> ${name} </h4>
+           <img class="avatar" src="${avatar}" />
+           <div class="health">health: <b> ${health} </b></div>
+           <div class="dice-container">    
+               ${diceHtml}
+           </div>
+       </div>`;
+}
+
+renderCharacter(hero);
+renderCharacter(monster);
