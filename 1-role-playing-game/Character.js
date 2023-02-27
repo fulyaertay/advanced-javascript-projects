dea0b8329a0fbc2a41a15c0e7c3e186d0bcecabc
Fulya <fulya.ertay@gmail.com>
2023-02-27 20:09:19 +0300

    refactored code

--- 1-role-playing-game/Character.js
+++ 1-role-playing-game/Character.js
@@ -1,20 +1,29 @@
-import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
+import { getDiceRollArray, getDicePlaceholderHtml, getPercentage } from './utils.js'
 
-const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth
+/*
+Challenge
+1. Convert the constructor function to a class.
 
-function Character(data) {
-    Object.assign(this, data)
-    this.maxHealth = this.health
+    Think about:
+    1. Where do properties go?
+    2. Where do Methods go?
+*/
 
-    this.diceArray = getDicePlaceholderHtml(this.diceCount)
 
-    this.getDiceHtml = function () {
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        this.maxHealth = this.health
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+    }
+
+    setDiceHtml() {
         this.currentDiceScore = getDiceRollArray(this.diceCount)
-        this.diceArray = this.currentDiceScore.map((num) =>
+        this.diceHtml = this.currentDiceScore.map((num) =>
             `<div class="dice">${num}</div>`).join("")
     }
 
-    this.takeDamage = function (attackScoreArray) {
+    takeDamage(attackScoreArray) {
         const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
         this.health -= totalAttackScore
         if (this.health <= 0) {
@@ -23,38 +32,17 @@ function Character(data) {
         }
     }
 
-/*
-CHALLENGE
-1. Instead of just logging the percent, getHealthBarHtml 
-needs to return this string of html:
-`<div class="health-bar-outer">
-    <div class="health-bar-inner *YOUR CODE HERE* " 
-        style="width: *YOUR CODE HERE* %;">
-    </div>
-</div>`
-2. You need to make some changes to that string of HTML.
-2a. If the amount of health left is 25% or lower, add the class 
-"danger" to the div with the class "health-bar-inner".
-2b. The width of that div should be the % health remaining. 
-3. Be sure to add the healthBar variable to the string of HTML
-rendered by getCharacterHtml.
-**hint.md for help!!**       
-*/
-
-    this.getHealthBarHtml = function () {
+    getHealthBarHtml() {
         const percent = getPercentage(this.health, this.maxHealth)
-        
-        return `
-        <div class="health-bar-outer">
-            <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
-            style="width: ${percent}%;">
-            </div>
-        </div>`
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
+                            style="width:${percent}%;">
+                    </div>
+                </div>`
     }
-    
 
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this
+    getCharacterHtml() {
+        const { elementId, name, avatar, health, diceCount, diceHtml } = this
         const healthBar = this.getHealthBarHtml()
         return `
             <div class="character-card">
@@ -63,7 +51,7 @@ rendered by getCharacterHtml.
                 <div class="health">health: <b> ${health} </b></div>
                 ${healthBar}
                 <div class="dice-container">
-                    ${this.diceArray}
+                    ${diceHtml}
                 </div>
             </div>`
     }
--- 1-role-playing-game/data.js
+++ 1-role-playing-game/data.js
@@ -4,13 +4,27 @@ const characterData = {
         avatar: "images/wizard.png",
         health: 60,
         diceCount: 3,
-        currentDiceScore: [],
+        currentDiceScore: []
     },
-    monster: {
+    orc: {
         name: "Orc",
         avatar: "images/orc.png",
-        health: 10,
+        health: 30,
         diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "images/demon.png",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "images/goblin.png",
+        health: 20,
+        diceCount: 3,
         currentDiceScore: []
     }
 }
