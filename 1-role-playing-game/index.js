c1949171a8a8ea52f9e46aa95c456dac850962e8
Fulya <fulya.ertay@gmail.com>
2023-02-27 15:19:15 +0300

    added endGame()

--- 1-role-playing-game/index.js
+++ 1-role-playing-game/index.js
@@ -1,21 +1,38 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
+/*
+CHALLENGE
+1. Inside attack(), check if either character is dead.
+If they are, call a new function called endGame().
+2. Set up the new function endGame() and have it 
+log out "the game is over".
+*/
+
 function attack() {
     wizard.getDiceHtml()
     orc.getDiceHtml()
     wizard.takeDamage(orc.currentDiceScore)
     orc.takeDamage(wizard.currentDiceScore)
     render()
+    if(wizard.dead || orc.dead){
+        endGame()
+    }
+}
+
+function endGame(){
+    console.log('The game is over')
 }
 
 
-function render() {
-    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
-}
+
 
 document.getElementById("attack-button").addEventListener('click', attack)
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml()
+}
 
 const wizard = new Character(characterData.hero)
 const orc = new Character(characterData.monster)
