d43e0c53b065fdb526c460b7499644fd512cc5af
Fulya <fulya.ertay@gmail.com>
2023-02-28 15:27:57 +0300

    added badges

--- 2-tindog-app/Dog.js
+++ 2-tindog-app/Dog.js
@@ -1,1 +1,22 @@
-// Create the Dog class here
+
+class Dog {
+    constructor(data) {
+        Object.assign(this, data)
+    }
+
+
+
+    getDogHtml() {
+        const { name, avatar, age, bio, hasBeenSwiped,hasBeenLiked  } = this
+        return `
+
+      <div class="image-text">
+        <h3 class="dog-name">${name}, ${age} </h3>
+        <h4 class="dog-info">${bio}</h4>
+      </div>
+    </div>`
+    }
+}
+
+export default Dog
+
