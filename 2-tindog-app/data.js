114909b02c868b2d370cac90641830a96eab0bdb
Fulya <fulya.ertay@gmail.com>
2023-02-28 13:41:54 +0300

    added dogs

--- 2-tindog-app/data.js
+++ 2-tindog-app/data.js
@@ -1,14 +1,14 @@
 const dogs = [
     {
         name: "Rex",
-        avatar: "images/dog-rex.jpg",
+        avatar: "./images/dog-rex.jpg",
         age: 25,
         bio: "Art. Literature. Natural wine. Yoga.",
         hasBeenSwiped: false,
         hasBeenLiked: false
     },{
         name: "Bella",
-        avatar: "images/dog-bella.jpg",
+        avatar: "./images/dog-bella.jpg",
         age: 43,
         bio: "Yup, that's my owner. U can meet him if you want",
         hasBeenSwiped: false,
@@ -16,7 +16,7 @@ const dogs = [
     },
     {
         name: "Teddy",
-        avatar: "images/dog-teddy.jpg",
+        avatar: "./images/dog-teddy.jpg",
         age: 30,
         bio: "How you doin?",
         hasBeenSwiped: false,
