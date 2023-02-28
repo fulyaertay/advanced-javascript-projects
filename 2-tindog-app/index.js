b2ac7c6980c30bbb0033fb5659cc0ac598cdab58
Fulya <fulya.ertay@gmail.com>
2023-02-28 15:28:02 +0300

    added badges

--- 2-tindog-app/index.js
+++ 2-tindog-app/index.js
@@ -1,1 +1,30 @@
-// Remember to import the data and Dog class!
+import dogs from './data.js'
+import Dog from './Dog.js'
+
+let dogsArray = ["Rex", "Bella", "Teddy"]
+let isWaiting = false
+
+function getNewDogs() {
+    const nextDogData = dogs[dogsArray.shift()]
+    return nextDogData ? new Dog(nextDogData) : {}
+}
+function crossedDog(){
+    document.querySelector(".badges").innerHTML = `<img src="./images/badge-nope.png" class="badge-nope" />`
+}
+
+function likedDog(){
+    document.querySelector(".badges").innerHTML = `<img src="./images/badge-like.png" class="badge-like" />`
+}
+
+
+
+
+document.querySelector(".cross-icon").addEventListener("click",crossedDog)
+document.querySelector(".like-icon").addEventListener("click",likedDog)
+function render() {
+    document.querySelector(".dog-image").innerHTML = firstDog.getDogHtml()
+}
+
+const firstDog = new Dog(dogs.Rex)
+let dog = getNewDogs()
+render()
